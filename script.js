2585339c29a77fbd0670ac709b6fceb30a25742b
Judex66 <sermac2000@mail.ru>
2024-06-12 19:12:22 +0300

    адаптация страницы

---
+++ script.js
@@ -0,0 +1,11 @@
+let more = document.querySelector('.button_modal');
+   let modal = document.querySelector('.modal');
+   let  close = document.querySelector('.close');
+    more.addEventListener('click', function() {
+        modal.style.display = 'block';
+        modal.classList.add('modal-animation');
+    });
+    close.addEventListener('click', function() {
+        modal.style.display = 'none';
+        modal.classList.remove('modal-animation');
+    });
